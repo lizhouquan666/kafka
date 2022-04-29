94593e0af86c8ae3be67ad21fc4deb5404029b6b
Master <994653587@qq.com>
2022-04-29 13:57:41 +0800

    4.29

--- src/main/java/com/lpd/kafkatest/controller/ProducerController.java
+++ src/main/java/com/lpd/kafkatest/controller/ProducerController.java
@@ -9,30 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProducerController {
 
     @Autowired
-   private KafkaTemplate<String,String> kafkaTemplate;
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     @RequestMapping("/producer")
-    public String machine()
-    {
-      //  String topic,String id,String msg
-      // kafkaTemplate.send(topic,id,msg);
-        kafkaTemplate.send("192.168.110.52","nima4554");
+    public String machine(String host, String msg) {
+        //  String topic,String id,String msg
+        // kafkaTemplate.send(topic,id,msg);
+        kafkaTemplate.send(host, msg);
 
         return "success";
     }
 
- /*   @RequestMapping("/producer40")
-    public String machine40(String msg)
-    {
-        kafkaTemplate.send("192.168.110.40",msg);
-        return "success";
-    }
-
-
-    @RequestMapping("/producer145")
-    public String machine145(String msg)
-    {
-        kafkaTemplate.send("192.168.110.145",msg);
-        return "success";
-    }
-*/
 }
